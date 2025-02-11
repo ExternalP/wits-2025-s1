131a235766d43bbd388bea3f4b3bba0024057213
Luis Alvarez <luis.21099592@gmail.com>
2025-02-11 12:02:28 +0800

    UserController creation and users migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('given_name');
+            $table->string('preferred_name');
+            $table->string('preferred_pronouns');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_photo')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
