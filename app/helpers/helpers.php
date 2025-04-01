641fa2f77e38965f149588fbb14c9c8a9ddb10c3
ExternalP <110000600+ExternalP@users.noreply.github.com>
2025-04-01 12:26:38 +0800

    Update helpers.php

--- app/helpers/helpers.php
+++ app/helpers/helpers.php
@@ -3,7 +3,7 @@
  * My custom helper functions.
  *  - Paste line below in file to use:
  *  require base_path("app/helpers/helpers.php");
- *  Functions: markdownToHtml(), assignableRoles(), assignableRolesNames().
+ *  Functions: assignableRoles(), assignableRolesNames().
  *
  *  Filename:        helpers.php
  *  Location:        app/helpers/
@@ -17,57 +17,6 @@
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Models\Role;
 
-
-/**
- * Provides formatting for the session flash.
- * - Supported Types: Info, Error, Warning, Success (case-insensitive)
- * @param  string  $type
- * @param  string  $message
- * @return array
- */
-function formatFlash(string $type = 'info', string $message = ''): array
-{
-    $flashArr = [
-        // "message" => "Something has happened.",
-        "message" => $message,
-        "icon" => "fa-solid fa-circle-info",
-        "type" => "Info",
-        "fgColour" => "text-white",
-        "bgColour" => "bg-sky-600",
-        "fgText" => "text-sky-900",
-        "bgText" => "bg-sky-100",
-    ];
-
-    switch (strtolower($type)) {
-        case 'error':
-            // $flashArr['message'] = "You Are Not Authorized To Perform this Action";
-            $flashArr['icon'] = "fa-solid fa-triangle-exclamation";
-            $flashArr['type'] = "Error";
-            // $flashArr['fgColour'] = "text-white";
-            $flashArr['bgColour'] = "bg-red-600";
-            $flashArr['fgText'] = "text-red-900";
-            $flashArr['bgText'] = "bg-red-100";
-            break;
-        case 'warning':
-            $flashArr['icon'] = "fa-solid fa-circle-exclamation";
-            $flashArr['type'] = "Warning";
-            // $flashArr['fgColour'] = "text-white";
-            $flashArr['bgColour'] = "bg-amber-600";
-            $flashArr['fgText'] = "text-amber-900";
-            $flashArr['bgText'] = "bg-amber-100";
-            break;
-        case 'success':
-            $flashArr['icon'] = "fa-solid fa-circle-check";
-            $flashArr['type'] = "Success";
-            // $flashArr['fgColour'] = "text-white";
-            $flashArr['bgColour'] = "bg-green-600";
-            $flashArr['fgText'] = "text-green-900";
-            $flashArr['bgText'] = "bg-green-100";
-            break;
-    }
-
-    return $flashArr;
-}
 
 // /**
 //  * Returns array of names of roles that the current user can create/edit/delete.
