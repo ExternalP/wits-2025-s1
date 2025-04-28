b80215388be4a5df0a4c943d4844000c5a6a8260
ExternalP <110000600+ExternalP@users.noreply.github.com>
2025-04-28 18:53:05 +0800

    feat(api): Setup for API stuff

---
+++ app/Http/Requests/v1/StoreCourseRequest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * DESCRIPTION_BUT_EACH_LINE_HAS_MAX_LENGTH_OF_96_CHARACTERS
+ *
+ * Filename:        StoreCourseRequest.php
+ * Location:        app/Http/Requests/v1/
+ * Project:         wits-2025-s1
+ * Date Created:    22/4/2025
+ *
+ * Author:          Corin Little <20135656@tafe.wa.edu.au>
+ *
+ */
+
+namespace App\Http\Requests\v1;
+
+use App\Models\Course;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Validation\Rule;
+
+class StoreCourseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        // $request['national_code'] = strtoupper($request['national_code']);
+        // $request['state_code'] = strtoupper($request['state_code']);
+        // if (!isset($request['cluster_id'])) $request['cluster_id'] = [];
+        // if (!isset($request['unit_id'])) $request['unit_id'] = [];
+
+        return [
+            'package_id' => ['required', 'integer', 'exists:packages,id',],
+            'national_code' => ['required', 'string', 'min:4', 'max:10', 'uppercase', 'alpha_num', /*Rule::unique('courses', 'national_code')*/],
+            'aqf_level' => ['required', 'string', 'min:1', 'max:100',],
+            'title' => ['required', 'string', 'min:1', 'max:255',],
+            'tga_status' => ['sometimes', 'nullable', 'string', Rule::in(Course::tgaStatuses())],
+            'state_code' => ['required', 'string', 'min:4', 'max:10', 'uppercase', 'alpha_num',],
+            'nominal_hours' => ['sometimes', 'nullable', 'integer', 'min:0', 'max:10000',],
+            'type' => ['sometimes', 'nullable', 'string', 'min:1', 'max:50',],
+            'cluster_id' => ['sometimes', 'exists:clusters,id',],
+            'unit_id' => ['sometimes', 'exists:units,id',],
+        ];
+    }
+}
+
