7ea3b95284aec4effe0fff417368f193ddc337b9
ExternalP <110000600+ExternalP@users.noreply.github.com>
2025-04-29 11:51:35 +0800

    docs(models): Cleaned up auto-generated comments

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -7,28 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
- * 
- *
  * @mixin IdeHelperPackage
- * @property int $id
- * @property string $national_code
- * @property string $title
- * @property string $tga_status
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Course> $courses
- * @property-read int|null $courses_count
- * @method static \Database\Factories\PackageFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Package newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Package newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Package query()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Package whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Package whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Package whereNationalCode($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Package whereTgaStatus($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Package whereTitle($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Package whereUpdatedAt($value)
- * @mixin \Eloquent
  */
 class Package extends Model
 {
