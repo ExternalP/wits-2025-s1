2146cef62175b7cbc2589cbf4d463587be9245d9
Chloe23077 <yunjasgu@gmail.com>
2025-06-03 11:04:39 +0800

    make pesttest for user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'given_name' => $this->faker->firstName,
+            'family_name' => $this->faker->lastName,
+            'preferred_name' => $this->faker->firstName,
+            'preferred_pronouns' => $this->faker->randomElement(['she', 'her', 'he', 'him']),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
